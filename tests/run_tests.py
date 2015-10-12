c10bb7e924baef87c3bdd7a8fa3e93e2ac63fd6d
Rodrigo Mariano <rodmariano13@gmail.com>
2015-10-11 22:56:08 -0300

    verificando se o teste está sendo executado dentro do Web2py ou Jenkins

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -8,35 +8,27 @@
 # Assim este arquivo de encarregará de rodar os testes na pasta tests/
 #
 
-
-# Salva os caminhos das pastas que serão testadas no sys.path
 from sys import path as sys_path
 from sys import exit as sys_exit
 from os import path as os_path
 sys_path_append=sys_path.append
 os_path_abspath=os_path.abspath
 
-# Localizando a pasta onde o web2py está para colocá-lo no sys.path
-W2P = '/web2py/applications/'
-JNKS = '/jenkins/jobs/'
+# Localizando a pasta onde o Web2py está para colocá-lo no sys.path
+W2P = '/web2py/applications/' # se estiver rodando dentro do Web2py, o caminho deve conter esta string
+JNKS = '/jenkins/jobs/' # se estiver rodando dentro do Jenkins, o caminho deve conter esta string
 PATH = os_path.sep.join(os_path.abspath(__file__).split(os_path.sep))
-print '\nPATH='+str(PATH)
-
 if W2P in PATH:
     W2P_PATH = os_path.sep.join(os_path.abspath(__file__).split(os_path.sep)[:-4])
 elif JNKS in PATH:
     W2P_PATH = os_path.sep.join(os_path.abspath(__file__).split(os_path.sep)[:-6])
     W2P_PATH = W2P_PATH+'/web2py'
 else:
-    raise Exception('Problem with the PATH. Please contact an administrator!')
+    raise Exception('Problem with the PATH. Please, contact an administrator!')
     sys_exit(1)
-
-#W2P_PATH = "/docs/projects/web2py" #o comando acima pegara este caminho
+#W2P_PATH = "/docs/projects/web2py"
 sys_path_append(os_path_abspath(W2P_PATH))
-print '\nW2P_PATH='+str(W2P_PATH)
 
-# sys.path.append(os.path.abspath(W2P_PATH+'/gluon'))
-# sys.path.append(os.path.abspath(W2P_PATH+'/site-packages'))
 # Pasta anterior a pasta atual (raiz projeto)
 PROJECT_PATH=os_path.sep.join(os_path_abspath(__file__).split(os_path.sep)[:-2])
 # Pasta atual, onde o run_tests.py está
