14567674732c39401b11911ecb9216333300e4db
Rodrigo Mariano <rodmariano13@gmail.com>
2015-12-08 20:16:32 -0200

    Arrumado o arquivo venv/run_tests.sh, colocando a função to_console

--- venv/run_tests.sh
+++ venv/run_tests.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
+# caso uma excecao ocorra antes do "echo", a excecao eh lancada para cima e faz
+# com que o jenkins entenda a excecao e reporte o problema
 set -e  # If occur any error, exit
 function to_console {
     echo -e "\n $1 \n"
 }
 
-
+# construcao do virtualenv, instacao das dependencias e execucao dos testes
 virtualenv venv/
-echo ""
-echo " --> Created the Virtualenv."
+to_console " --> Created the Virtualenv."
 
 source venv/bin/activate
-echo ""
-echo " --> Activated the Virtualenv."
-echo ""
+to_console " --> Activated the Virtualenv."
 
 pip install -r venv/requirements.txt
-echo ""
-echo " --> Installed the requirements."
-echo ""
+to_console " --> Installed the requirements."
 
 python tests/run_tests.py
-
-to_console " --> Ran the tests in tests/run_tests.py"
+to_console " --> Ran the tests in tests/run_tests.py."
